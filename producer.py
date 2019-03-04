b889e3577c162a5a8cd895b5592286d71ab88dd5
vladaindjic <vladaindjic@icloud.com>
2019-03-04 01:55:46 +0100

    simple version of kafka producer and log generator

---
+++ producer.py
@@ -0,0 +1,34 @@
+import socket
+
+from kafka import KafkaProducer
+
+MAX_MESSAGE_SIZE = 4096
+PORT = 50000
+
+
+def main():
+    # kafka cluster listens on 9094
+    # we should encode data using utf-8
+    producer = KafkaProducer(bootstrap_servers=['localhost:9094'],
+                             value_serializer=lambda x:
+                             x.encode('utf-8'))
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('localhost', PORT))
+    s.listen(1)
+    print("Producer starts")
+    while True:
+        # accpet message from log generator and send message to kafka cluster
+        conn, addr = s.accept()
+        data = conn.recv(MAX_MESSAGE_SIZE)
+        if not data:
+            break
+        data_str = data.decode('utf-8')
+        print(data_str)
+        producer.send('test', value=data_str)
+    conn.close()
+    s.close()
+
+
+if __name__ == '__main__':
+    main()
