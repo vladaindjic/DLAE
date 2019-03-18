cd53886f4cda377128eb41f0d5de87b10a51f900
vladaindjic <vladaindjic@icloud.com>
2019-03-18 23:05:24 +0100

    semantic analysis; generating log parser; parsing logs

--- log_formatter.py
+++ log_formatter.py
@@ -1,5 +1,6 @@
 from parglare import Grammar, Parser
 from collections import OrderedDict
+import dateutil.parser as date_parser
 import re
 
 INT_REGEX_PATTERN = r'[+-]?\d+'
@@ -13,11 +14,63 @@ STRING_TYPE = "STRING"
 DATE_TIME_TYPE = "DATE_TIME"
 
 
+class Log:
+    pass
+
+
 class LogParser:
-    def __init__(self):
-        self._declarations = OrderedDict()
-        self._ids = set()
-        self._elements = []
+    def __init__(self, declarations, elements):
+        self._declarations = declarations
+        self._elements = elements
+
+    def parse_log(self, text):
+        unmatched = text
+        log = Log()
+        for el in self._elements:
+            if isinstance(el.content, Regex):
+                matched, unmatched = self._match_regex(el.content, unmatched, text)
+            else:
+                if isinstance(el.content, Identifier):
+                    declaration = self._declarations[el.content.id]
+                elif isinstance(el.content, Declaration):
+                    declaration = el.content
+                else:
+                    raise ValueError('This should not happen')
+
+                identifier, datatype, regex = declaration.identifier, declaration.type, declaration.regex
+                matched, unmatched = self._match_regex(regex, unmatched, text)
+
+                value = self._create_value_for_type(matched, datatype)
+                # FIXME: bad, but easy way to add attribute to object
+                log.__dict__[identifier.id] = value
+
+        if unmatched:
+            raise ValueError('There is unmathced characters: (%s) in log: %s' % (unmatched, text))
+
+        return log
+
+    @staticmethod
+    def _match_regex(regex, unmatched, text):
+        m = re.match(regex.regex, unmatched)
+        if m is None:
+            raise ValueError(
+                "Regex: /%s/ is not matched at this part: %s of the log: %s" % (
+                    regex.regex.pattern, unmatched, text))
+        start, end = m.span()
+        return unmatched[start:end], unmatched[end:]
+
+    @staticmethod
+    def _create_value_for_type(matched_str, datatype):
+        if datatype == INT_TYPE:
+            return int(matched_str)
+        elif datatype == DOUBLE_TYPE:
+            return float(matched_str)
+        elif datatype == STRING_TYPE:
+            return str(matched_str)
+        elif datatype == DATE_TIME_TYPE:
+            return date_parser.parse(matched_str)
+        else:
+            raise TypeError('Wrong datetype: %s' % datatype)
 
 
 # IR
@@ -29,15 +82,16 @@ def create_regex_object(regex, default_regex_pattern):
     :return:
     """
     if regex is None:
-        return Regex(re.compile(default_regex_pattern))
+        # slashes are added, beacuse constructor of Regex class will remove threm
+        return Regex('/' + default_regex_pattern + '/')
     return regex
 
 
 class Declaration:
     def __init__(self, identifier, datatype, regex):
-        self.id = identifier
+        self.identifier = identifier
         self.type = datatype
-        print(regex)
+        # print(regex)
         self.regex = regex
 
 
@@ -66,6 +120,45 @@ class LogFormat:
         self.declarations = declarations
         self.body = body
 
+    def semantic_analysis(self):
+        decl_dict = OrderedDict()
+        body_decl_dict = OrderedDict()
+        id_set = set()
+        # identifiers in declarations must be unique
+        if self.declarations is not None:
+            for decl in self.declarations:
+                if decl.identifier.id in decl_dict:
+                    raise ValueError("Declaration for id: %s is not unique" % decl.identifier.id)
+                decl_dict[decl.identifier.id] = decl
+        # each element should have unique id (FIXME: should this be fixed)
+        # each id in declarations places in body should be unique too
+        for el in self.body:
+            if isinstance(el.content, Declaration):
+                declaration = el.content
+                if declaration.identifier.id in decl_dict or declaration.identifier.id in body_decl_dict:
+                    raise ValueError("Declaration placed in body for id: %s is not unique" % declaration.identifier.id)
+                body_decl_dict[declaration.identifier.id] = declaration
+            elif isinstance(el.content, Regex):
+                pass
+            elif isinstance(el.content, Identifier):
+                identifier = el.content
+                # element id should be unique in body
+                if identifier.id in body_decl_dict:
+                    raise ValueError('Id: %s is used in declaration placed in body.' % identifier.id)
+                elif identifier.id not in decl_dict:
+                    raise ValueError('Id: %s is not declared.' % identifier.id)
+                elif identifier.id in id_set:
+                    raise ValueError('Element with id: %s is put multiple times in body.' % identifier.id)
+                id_set.add(identifier.id)
+            else:
+                raise TypeError('Wrong value of body element')
+
+        # merge dicts with declarations
+        for k in body_decl_dict:
+            decl_dict[k] = body_decl_dict[k]
+
+        return LogParser(decl_dict, self.body.elements)
+
 
 class Declarations:
     def __init__(self):
@@ -76,6 +169,9 @@ class Declarations:
         # so we could propagate declarations
         return self
 
+    def __iter__(self):
+        return (yield from self.declarations)
+
 
 class Body:
     def __init__(self):
@@ -85,6 +181,9 @@ class Body:
         self.elements.append(element)
         # so we could propagate body
         return self
+
+    def __iter__(self):
+        return (yield from self.elements)
 
 
 class Element:
@@ -99,7 +198,7 @@ class Identifier:
 
 class Regex:
     def __init__(self, regex):
-        self.regex = re.compile(regex)
+        self.regex = re.compile(regex[1:-1])
 
 
 def create_declaration(identifier, rhsdecl_tupple):
@@ -202,7 +301,7 @@ actions = {
     # STRING: 'string';
     "STRING": lambda _, value: STRING_TYPE,
     # DATETIME: 'datetime';
-    "DATETIME": lambda _value: DATE_TIME_TYPE
+    "DATETIME": lambda _, value: DATE_TIME_TYPE
     # SEMICOLON: ';';
 
 }
@@ -210,17 +309,29 @@ actions = {
 primer = """
     p:=int; 
     a:=double; 
-    b:=double; 
-    <asd := int(/\./)> </\..*/> <p>
+    b:=double;
+    c:=datetime(/\d{2}\.\d{2}\.\d{4}/);
+    <a> </,\s+/> <b> </,\s+/> <c> </;\s+/> <p>
     """
+log = '3.1, -3.5, 13.03.1905;   3000'
+
+
+# primer = '''
+#     </,\s+/> </asd/>
+# '''
+# log = ",   asd  "
 
 
 def main():
     g = Grammar.from_file('log_formatter.pg')
     p = Parser(g, actions=actions)
-    res = p.parse(primer)
-    print(res)
-
+    lf = p.parse(primer)
+    lp = lf.semantic_analysis()
+    l = lp.parse_log(log)
+    print(l.a)
+    print(l.b)
+    print(l.c)
+    print(l.p)
 
 if __name__ == '__main__':
     main()
